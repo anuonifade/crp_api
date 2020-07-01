5592fc5d32857bbb71b5bca449038e5a13823529
andela-aonifade <anuoluwapo.onifade@andela.com>
2020-07-01 17:47:14 +0100

    implements user signup, authentication and user signin

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -1,29 +1,67 @@
 class Api::V1::UserController < ApplicationController
+  include Response
+  include ExceptionHandler
+  skip_before_action :authorize_request, only: [:create, :index, :authenticate]
 
   def index
     redirect_to '/api-docs'
   end
 
   def create
+    user_data = user_params.merge({
+      'referral_token' => SecureRandom.alphanumeric(16)
+    })
+    if params[:referral_token].present?
+      referral = find_referral(params[:referral_token])
+      user = referral.user.create(user_data)
+    else
+      user = User.create(user_data)
+    end
 
+    if user.persisted?
+      token = AuthenticateUser.new(user.email, user.password).call
+      response = { message: Message.account_created, auth_token: token }
+      json_response(response, :created)
+    end
   end
 
   def authenticate
-  end
-
-  def create_with_referral
+    token = AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
+    json_response(auth_token: token)
   end
 
   def invite_users
+
   end
 
   def show
   end
 
   def referral_link
+
   end
 
   def invited_users
+    binding.pry
+    @invited_users = current_user.users.all
+    json_response(@invited_users)
   end
 
+  private
+
+  def user_params
+    params.permit(:first_name, :last_name, :email, :password, :referral_token)
+  end
+
+  def auth_params
+    params.permit(:email, :password)
+  end
+
+  def find_referral(referral_token)
+    User.find_by(referral_token: referral_token)
+  end
+
+  def authorize
+    user_id = JsonWebToken.decode()
+  end
 end
--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -14,39 +14,8 @@ class JsonWebToken
     body = JWT.decode(token, HMAC_SECRET)[0]
     HashWithIndifferentAccess.new body
     # rescue from any error encountered during decoding
-  rescue JWT::DecodeError => begin
+  rescue JWT::DecodeError => e
     # Raise Custom error to be handled by handler
     raise ExceptionHandler::InvalidToken, e.message
-  end
-
-  module ExceptionHandler
-    extend ActiveSupport::Concern
-
-    class AuthenticationError < StandardError; end
-    class MissingToken < StandardError; end
-    class InvalidToken < StandardError; end
-
-    included do
-      rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
-      rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
-      rescue_from ExceptionHandler::MissingToken, with: :four_twenty_two
-      rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
-
-      rescue_from ActiveRecord::RecordNotFound do |e|
-        json_response({message: e.message }, :not_found)
-      end
-    end
-
-    private
-
-    # JSON response with Status code 422 - unprocessable entity
-    def four_twenty_two(e)
-      json_response({ message: e.message }, :unprocessable_entity)
-    end
-
-    #JSON response with Status code 401 - Unauthorized
-    def unauthorized_request(e)
-      json_response({ message: e.message }, :unauthorized)
-    end
   end
 end
