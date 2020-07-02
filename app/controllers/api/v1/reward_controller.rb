3956c3a9a360e512976c772e7051ca74201fd9dd
andela-aonifade <anuoluwapo.onifade@andela.com>
2020-07-02 02:31:02 +0100

    adds rewards for user that registered with referral invites and referral with registered user of up to 5

--- app/controllers/api/v1/reward_controller.rb
+++ app/controllers/api/v1/reward_controller.rb
@@ -1,6 +1,12 @@
 class Api::V1::RewardController < ApplicationController
   
   def index
+    rewards = current_user.rewards.select(:id, :amount, :status).all
 
+    total_rewards = rewards.sum(:amount) if rewards.present? 
+    json_response({
+      rewards: rewards,
+      total_rewards: total_rewards
+    })
   end
 end
